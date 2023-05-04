2cc151e6d2aff896c502796eb2c81fbf98897102
Des117 <tugiadinh117@gmail.com>
2023-05-04 17:41:39 +0700

    Show list of orders, have pop up menu for full detail (not show full detail yet)

---
+++ .history/Asset/JS/shipper_20230504173101.js
@@ -0,0 +1,15 @@
+const $ = document.querySelector.bind(document);
+const mdl = $('#info-popup');
+
+function popupFunction(e) {
+  const targ = e.target;
+  const prnt = targ.closest('div.order');
+  const chldn = prnt.childNodes;
+  const txt = [...chldn].filter((d) => d.className === 'popupText');
+  const msg = txt.childNodes;
+  $('#info-popup .body').childNodes = msg;
+  $('#info-popup').classList.add('info-popup-data');
+}
+function closeModal(e){
+  e.target.classList.remove('info-popup-data');
+}
