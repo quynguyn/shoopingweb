393169d9e424dcd0ef185b17af2e5891627f375e
Des117 <tugiadinh117@gmail.com>
2023-05-04 22:57:21 +0700

    move close to button, add gitignore

--- Asset/JS/shipper.js
+++ Asset/JS/shipper.js
@@ -2,14 +2,19 @@ const $ = document.querySelector.bind(document);
 const mdl = $('#info-popup');
 
 function popupFunction(e) {
-  const targ = e.target;
-  const prnt = targ.closest('div.order');
-  const chldn = prnt.childNodes;
-  const txt = [...chldn].filter((d) => d.className === 'popupText');
-  const msg = txt.childNodes;
-  $('#info-popup .body') = txt;
-  $('#info-popup').classList.add('info-popup-data');
+	const target = e.target;
+	const parent = target.closest('div.order');
+	const childs = parent.childNodes;
+	var data = childs[3].childNodes;
+	const txt = [...data].filter((d) => d.className === 'information');
+
+	for (var i = 0; i < txt.length; i++) {
+		$('#info-popup .body').innerHTML += txt[i].innerText + '<br>';
+	}
+	$('#info-popup').classList.add('info-popup-data');
 }
-function closeModal(e){
-  e.target.classList.remove('info-popup-data');
+function closeModal(e) {
+	$('#info-popup .body').innerHTML = '';
+
+	$('#info-popup').classList.remove('info-popup-data');
 }
