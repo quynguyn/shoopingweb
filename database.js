842ca2cba01eb3243bc2bdf9c15ad76c4fbc8354
quynguyn <s3878636@rmit.edu.vn>
2023-05-08 14:47:02 +0700

    using database

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const { MongoClient } = require('mongodb');
 const uri = 'mongodb+srv://quynguyen:xfqM6RcvtWw22Ozf@lazadaclone.cqg5ikw.mongodb.net/';
 
 const dbName = 'lazadaWebsite';
-  
+
 const client = new MongoClient(uri);
 
 async function main() {
@@ -18,7 +18,7 @@ async function main() {
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('documents');
+  const collection = db.collection('products');
   
   console.log(db.client)
   // the following code examples can be pasted here...
@@ -30,3 +30,27 @@ main()
   .then(console.log)
   .catch(console.error)
   .finally(() => client.close());
+
+
+//   const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority";
+
+// const client = new MongoClient(uri);
+
+// client.connect(err => {
+//   if (err) throw err;
+
+//   const database = client.db("<database>");
+
+//   database.listCollections().toArray((err, collections) => {
+//     if (err) throw err;
+
+//     console.log(`Collections in ${database.databaseName}:`);
+
+//     collections.forEach(collection => {
+//       console.log(collection.name);
+//     });
+
+//     client.close();
+//   });
+// });
