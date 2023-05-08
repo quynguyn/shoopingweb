9f4d187b8ddbf4de4726aa27f5ff82e0a2a34694
quynguyn <s3878636@rmit.edu.vn>
2023-05-08 23:54:50 +0700

    add new package to the project

--- database.js
+++ database.js
@@ -5,52 +5,33 @@
 // const dbName = '';
 
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 // Replace the placeholder values with your MongoDB connection string and database name
 const uri = 'mongodb+srv://quynguyen:xfqM6RcvtWw22Ozf@lazadaclone.cqg5ikw.mongodb.net/';
 
 const dbName = 'lazadaWebsite';
 
+mongoose.connect(uri);
+
 const client = new MongoClient(uri);
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('products');
-  
-  console.log(db.client)
-  // the following code examples can be pasted here...
+async function run() {
+  try {
+    await client.connect();
+    // database and collection code goes here
+    const db = client.db("dbName");
+    const coll = db.collection("products");
+    // find code goes here
+    const cursor = coll.find({ hasRings: true });
+    // iterate code goes here
+    await cursor.forEach(console.log);
+  } 
 
-  return 'done.';
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
-
-
-//   const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority";
-
-// const client = new MongoClient(uri);
-
-// client.connect(err => {
-//   if (err) throw err;
-
-//   const database = client.db("<database>");
-
-//   database.listCollections().toArray((err, collections) => {
-//     if (err) throw err;
-
-//     console.log(`Collections in ${database.databaseName}:`);
-
-//     collections.forEach(collection => {
-//       console.log(collection.name);
-//     });
-
-//     client.close();
-//   });
-// });
+run().catch(console.dir);
