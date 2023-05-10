562993b880bddc991c2e75323440594dba11a62b
Des117 <tugiadinh117@gmail.com>
2023-05-10 13:27:50 +0700

    major fix customer.html, test some database

--- Asset/JS/shipper.js
+++ Asset/JS/shipper.js
@@ -1,20 +1,19 @@
 const $ = document.querySelector.bind(document);
-const mdl = $('#info-popup');
+const infoDialog = document.querySelector(".info-modal");
 
-function popupFunction(e) {
-	const target = e.target;
-	const parent = target.closest('div.order');
-	const childs = parent.childNodes;
-	var data = childs[3].childNodes;
-	const txt = [...data].filter((d) => d.className === 'information');
+function popupFunction() {
+	// const target = e.target;
+	// const parent = target.closest('div.order');
+	// const childs = parent.childNodes;
+	// var data = childs[3].childNodes;
+	// const txt = [...data].filter((d) => d.className === 'information');
 
-	for (var i = 0; i < txt.length; i++) {
-		$('#info-popup .body').innerHTML += txt[i].innerText + '<br>';
-	}
-	$('#info-popup').classList.add('info-popup-data');
+	// for (var i = 0; i < txt.length; i++) {
+	// 	$('#info-popup .body').innerHTML += txt[i].innerText + '<br>';
+	// }
+	// $('#info-dialog').classList.add('info-popup-data');
+	infoDialog.showModal();
 }
-function closeModal(e) {
-	$('#info-popup .body').innerHTML = '';
-
-	$('#info-popup').classList.remove('info-popup-data');
+function closeModal() {
+	infoDialog.close();
 }
---
+++ app.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
+// Mongoose connection
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/nodemongo');
+const db = mongoose.connection;
+
+const posts = require('./routes/posts');
+app.use('/posts', posts);
+
+// Check for DB connection
+db.once('open', function () {
+	console.log("Connected to MongoDB successfully!");
+});
+
+db.on('error', function () {
+	console.log("err");
+});
+
+// Route for home
+app.get('/hello', function (req, res) {
+	res.send('hello world')
+});
+
+// Start server with port 3000
+app.listen(3000, function () {
+	console.log("Server started on localhost:3000");
+});
+
+module.exports = app;
