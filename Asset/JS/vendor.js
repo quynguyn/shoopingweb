f7549e8693255e27e5cdaffb0917f8974b5af2f8
Des117 <tugiadinh117@gmail.com>
2023-05-11 22:07:41 +0700

    fix confuse database fecth, major fix vendor

--- Asset/JS/vendor.js
+++ Asset/JS/vendor.js
@@ -1,103 +1,141 @@
 const itemBoxTemplate = document.querySelector("[item-box-template]")
 const itemBoxContainer = document.querySelector("[item-box-container]")
 
-fetch('http://localhost:3000/accounts')
-    .then(res => res.json())
-    .then(data => {
-        products = data.map(product => {
-            const box = itemBoxTemplate.content.cloneNode(true).children[0]
-            const name = box.querySelector("[item-name]")
-            const price = box.querySelector("[item-price]")
-            const image = box.querySelector("[item-image]")
-            const description = box.querySelector("[item-description]")
-            name.textContent = product.name
-            price.textContent = product.price
-            image.Content = product.image
-            description.textContent = product.description
-            itemBoxContainer.append(box)           
-            return {name: product.name,price: product.price,description: product.description, element: box}
-        })
-    })
+
+const detailModal = document.querySelector(".detail-dialog")
+
+function showData(position) {
+	detailModal.showModal()
+
+	const detailTemplate = document.getElementById('detail-template');
+	const detailContainer = document.querySelector('.detail-body');
+
+	const templateClone = detailTemplate.content.cloneNode(true);
+
+	const productImage = templateClone.querySelector('.product-image');
+	const productName = templateClone.querySelector('.product-name');
+	const productPrice = templateClone.querySelector('.price');
+	const productDescription = templateClone.querySelector('.description');
+
+	productImage.src = productData[position].image;
+	productName.textContent = productData[position].name;
+	productPrice.textContent = '$' + productData[position].price;
+	productDescription.textContent = productData[position].description;
+
+	// console.log(products[position]);
+
+
+	detailContainer.appendChild(templateClone);
+}
+
+const closeDataButton = document.querySelector("[closeData]");
+
+closeDataButton.addEventListener('click', () => {
+	document.querySelector('.detail-body').innerHTML = '';
+
+	detailModal.close()
+});
+
+var productData;
+
+fetch('http://localhost:3000/products')
+	.then(res => res.json())
+	.then(data => {
+		var i = 0;
+		data.map(product => {
+			const box = itemBoxTemplate.content.cloneNode(true).children[0]
+			box.classList = i + ' ' + box.classList
+			box.onclick = () => showData(box.classList[0])
+			i++
+			const name = box.querySelector("[item-name]")
+			const price = box.querySelector("[item-price]")
+			const image = box.querySelector("[item-image]")
+			const description = box.querySelector("[item-description]")
+			name.textContent = product.name
+			price.textContent = product.price
+			image.querySelector("img").src = product.image
+			// description.textContent = product.description
+
+			itemBoxContainer.append(box)
+			return { name: product.name, price: product.price, description: product.description, element: box }
+		})
+
+		productData = data;
+	})
 
 // Open html dialog 
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
 
-  console.log(document.querySelector("[data-open-modal]"))
-
 openButton.addEventListener('click', () => {
-    console.log("run")
-    modal.showModal()
+	modal.showModal()
 });
 
 closeButton.addEventListener('click', () => {
-    modal.close()
+	modal.close()
 });
 
 // submit data form modal
 const form = document.querySelector('.inputData');
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault(); // prevent the default form submission
+form.addEventListener('submit', function (e) {
+	e.preventDefault(); // prevent the default form submission
 
-  var newProduct = {
-    nameValue: "",
-    priceValue: 0,
-    imageFile: "",
-    descriptionValue: "",
-  };
+	var newProduct = {
+		nameValue: "",
+		priceValue: 0,
+		imageFile: "",
+		descriptionValue: "",
+	};
 
+	const nameValue = document.getElementById('name').value;
+	const priceValue = Number(document.getElementById('price').value); // convert to number
+	const imageFile = document.getElementById('image').value; // get the first file selected (if any)
+	const descriptionValue = document.getElementById('description').value;
 
-  const nameValue = document.getElementById('name').value;
-  const priceValue = Number(document.getElementById('price').value); // convert to number
-  const imageFile = document.getElementById('image').value; // get the first file selected (if any)
-  const descriptionValue = document.getElementById('description').value;
+	newProduct.nameValue = nameValue;
+	newProduct.priceValue = priceValue;
+	newProduct.imageFile = imageFile;
+	newProduct.descriptionValue = descriptionValue;
 
-  newProduct.nameValue = nameValue;
-  newProduct.priceValue = priceValue;
-  newProduct.imageFile = imageFile;
-  newProduct.descriptionValue = descriptionValue;
+	localStorage.setItem('newProduct', JSON.stringify(newProduct));
 
-  localStorage.setItem('newProduct', JSON.stringify(newProduct));
-  
-  newProductBox(nameValue, priceValue, imageFile, descriptionValue)
-  console.log(newProductBox)
-  // reset the form
-  form.reset();
+	newProductBox(nameValue, priceValue, imageFile, descriptionValue)
+	// reset the form
+	form.reset();
 });
 
 // display new product dynamic
-function newProductBox
-(nameValue, priceValue, imageFile, descriptionValue){
+function newProductBox(nameValue, priceValue, imageFile, descriptionValue) {
 
-const box = document.createElement("div");
-box.classList.add("box");
+	const box = document.createElement("div");
+	box.classList.add("box");
 
-const name = document.createElement("div");
-name.classList.add("name");
-name.textContent = nameValue;
+	const name = document.createElement("div");
+	name.classList.add("name");
+	name.textContent = nameValue;
 
-const price = document.createElement("div");
-price.classList.add("price");
-price.textContent = priceValue;
+	const price = document.createElement("div");
+	price.classList.add("price");
+	price.textContent = priceValue;
 
-const imageContainer = document.createElement("div");
-imageContainer.classList.add("image");
-const image = document.createElement("img");
-image.src = imageFile;
-image.alt = nameValue;
-imageContainer.appendChild(image);
+	const imageContainer = document.createElement("div");
+	imageContainer.classList.add("image");
+	const image = document.createElement("img");
+	image.src = imageFile;
+	image.alt = nameValue;
+	imageContainer.appendChild(image);
 
-const description = document.createElement("div");
-description.classList.add("description");
-description.textContent = descriptionValue;
+	const description = document.createElement("div");
+	description.classList.add("description");
+	description.textContent = descriptionValue;
 
-box.appendChild(name);
-box.appendChild(price);
-box.appendChild(imageContainer);
-box.appendChild(description);
-box.classList.add('hover')
+	box.appendChild(name);
+	box.appendChild(price);
+	box.appendChild(imageContainer);
+	box.appendChild(description);
+	box.classList.add('hover')
 
-container.appendChild(box);
+	container.appendChild(box);
 }
