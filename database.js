42af49327a103a1439131bb4faec9a42faea5b70
quynguyn <s3878636@rmit.edu.vn>
2023-05-15 13:26:33 +0700

    create new account for the database

--- database.js
+++ database.js
@@ -111,6 +111,15 @@ app.get('/accounts/:id', (req, res) => {
 });
 
 
+// CREATE - Create a new product
+app.post('/accounts', (req, res) => {
+	console.log(req.body);
+	const account = new Account(req.body);
+	account.save()
+	  .then(() => res.redirect('/accounts'))
+	  .catch(error => res.send(error));
+  });
+  
   
 // -----------------------Distribution Hub-----------------------
 app.get('/distributionHubs', (req, res) => {
