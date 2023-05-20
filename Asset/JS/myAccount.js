1a89bed1726ab7553f2bd71e3b2cc2f557fba064
Des117 <tugiadinh117@gmail.com>
2023-05-20 16:22:34 +0700

    Update myAccount.js

--- Asset/JS/myAccount.js
+++ Asset/JS/myAccount.js
@@ -38,22 +38,6 @@ const distributionHub = document.querySelector('#distributionHub')
 
 const form = document.getElementById("account-information")
 
-const usernameInput = document.getElementById("username");
-usernameInput.onfocus = function () {
-	document.querySelector("#username-message").style.display = "block";
-}
-usernameInput.onblur = function () {
-	document.querySelector("#username-message").style.display = "none";
-}
-
-const passwordInput = document.querySelector("#password");
-passwordInput.onfocus = function () {
-	document.querySelector("#password-message").style.display = "block";
-}
-passwordInput.onblur = function () {
-	document.querySelector("#password-message").style.display = "none";
-}
-
 fillData()
 
 function fillData() {
@@ -92,3 +76,68 @@ function togglePasswordVisibility() {
 		toggleButton.textContent = "⊼";
 	}
 }
+
+const usernameInput = document.getElementById("username");
+usernameInput.onfocus = function () {
+	document.querySelector("#username-message").style.display = "block";
+}
+usernameInput.onblur = function () {
+	document.querySelector("#username-message").style.display = "none";
+}
+usernameInput.addEventListener("input", (e) => {
+	const username = e.target.value
+	var letter = document.querySelector("#username-message #u-letter");
+	var number = document.querySelector("#username-message #u-number");
+	var length = document.querySelector("#username-message #u-length");
+
+	const lowerCaseLetters = /[a-zA-z]/g;
+	const numbers = /[0-9]/g;
+	const minimumLength = (username.length >= 8) && (username.length <= 15)
+
+	letter.classList.toggle("valid", username.match(lowerCaseLetters));
+	letter.classList.toggle("invalid", !username.match(lowerCaseLetters));
+
+	number.classList.toggle("valid", username.match(numbers));
+	number.classList.toggle("invalid", !username.match(numbers));
+
+	length.classList.toggle("valid", minimumLength);
+	length.classList.toggle("invalid", !minimumLength);
+});
+
+const passwordInput = document.querySelector("#password");
+passwordInput.onfocus = function () {
+	document.querySelector("#password-message").style.display = "block";
+}
+passwordInput.onblur = function () {
+	document.querySelector("#password-message").style.display = "none";
+}
+
+passwordInput.addEventListener("input", (e) => {
+	const password = e.target.value
+	var letter = document.querySelector("#password-message #p-letter");
+	var capital = document.querySelector("#password-message #p-capital");
+	var number = document.querySelector("#password-message #p-number");
+	var special = document.querySelector("#password-message #p-special");
+	var length = document.querySelector("#password-message #p-length");
+
+	const lowerCaseLetters = /[a-z]/g;
+	const upperCaseLetters = /[A-Z]/g;
+	const numbers = /[0-9]/g;
+	const specialLetters = /[!@#$%^&]/g;
+	const minimumLength = (password.length >= 8) && (password.length <= 20)
+
+	letter.classList.toggle("valid", password.match(lowerCaseLetters));
+	letter.classList.toggle("invalid", !password.match(lowerCaseLetters));
+
+	capital.classList.toggle("valid", password.match(upperCaseLetters));
+	capital.classList.toggle("invalid", !password.match(upperCaseLetters));
+
+	number.classList.toggle("valid", password.match(numbers));
+	number.classList.toggle("invalid", !password.match(numbers));
+
+	special.classList.toggle("valid", password.match(specialLetters));
+	special.classList.toggle("invalid", !password.match(specialLetters));
+
+	length.classList.toggle("valid", minimumLength);
+	length.classList.toggle("invalid", !minimumLength);
+});
